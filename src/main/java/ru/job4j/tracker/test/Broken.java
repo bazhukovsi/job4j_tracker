c95c5af78d667c89eadfe33fb620c99032d06b16
bazhukovsi <bazhukobsi@yandex.ru>
2025-04-02 13:19:41 +0300

    6. Правила Checkstyle [#504981 #538204] Version2

---
+++ src/main/java/ru/job4j/tracker/test/Broken.java
@@ -0,0 +1,24 @@
+package ru.job4j.tracker.test;
+
+public class Broken {
+    private int sizeOfEmpty = 10;
+    private String name;
+    private String surname;
+    private static final String NEWVALUE = "";
+
+    public Broken() {
+    }
+
+    public void echo() {
+    }
+
+    public void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
+
+    }
+}
